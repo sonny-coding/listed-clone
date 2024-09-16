22f1856e49e76fbfad0e666f427527661f398d96
Son Dao <udemydaohsn@gmail.com>
2024-09-16 16:11:40 -0700

    ready

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -3,20 +3,19 @@ import Game from "./Game";
 import { property } from "@/lib/data";
 
 // fetch from supabase
-// const fetchRandomProperty = async () => {
-//   await new Promise((resolve) => {
-//     setTimeout(resolve, 10);
-//   });
-//   return property;
-// };
+const fetchRandomProperty = async () => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 10);
+  });
+  return property;
+};
 
 const Page = async () => {
-  //   const data = await fetchRandomProperty();
+  const data = await fetchRandomProperty();
 
   return (
     <>
-      {/* <Game property={property} />; */}
-      <div>hi</div>;
+      <Game property={property} />;
     </>
   );
 };
